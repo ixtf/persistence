c602c0a7e4594d6c52ab1817bcd90dcc5ff2e174
ixtf <ixtf1984@gmail.com>
2020-05-13 21:29:19 +0800

    fix to db value collection in embeddable

--- core/src/main/java/com/github/ixtf/persistence/api/writer/DefaultValueWriter.java
+++ core/src/main/java/com/github/ixtf/persistence/api/writer/DefaultValueWriter.java
@@ -3,6 +3,7 @@ package com.github.ixtf.persistence.api.writer;
 import com.github.ixtf.persistence.api.ValueWriter;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * @author jzb 2019-02-15
@@ -13,7 +14,14 @@ public class DefaultValueWriter implements ValueWriter<Object, Object> {
     public <T> boolean isCompatible(Class<T> clazz) {
         return String.class.isAssignableFrom(clazz)
                 || BigDecimal.class.isAssignableFrom(clazz)
+                || Date.class.isAssignableFrom(clazz)
+                || short.class.isAssignableFrom(clazz)
+                || Short.class.isAssignableFrom(clazz)
+                || int.class.isAssignableFrom(clazz)
                 || Integer.class.isAssignableFrom(clazz)
+                || float.class.isAssignableFrom(clazz)
+                || Float.class.isAssignableFrom(clazz)
+                || long.class.isAssignableFrom(clazz)
                 || Long.class.isAssignableFrom(clazz);
     }
 
